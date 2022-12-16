66bdf0ddba906ba2ed889e0037d3efd741b2c10d
pete02 <petrik.rasanen@gmail.com>
2022-12-16 16:54:15 +0200

    created frontend

--- test.js
+++
@@ -1,7 +0,0 @@
-const sharp =require( 'sharp');
-const fs=require('fs');
-const pic=fs.readFile("C:\Users\petri\Desktop\image.jpg", function(err, data){
-      
-  // Display the file content
-  console.log(data);
-})
